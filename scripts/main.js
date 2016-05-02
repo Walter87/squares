6344427c4406abad40362b25e7ce9d1d58901b95
Grzegorz <gb87@wp.pl>
2016-05-02 16:56:48 +0200

    Prepare grid of square divs 16x16

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,8 @@
 $(document).ready(function(){
-  width = $('.container').width();
-  alert(width);
+  width = $('.container').width()/16-6;
+
+  for (i=1; i <= Math.pow(16,2) ; i++) {
+  $('.container').append("<div style = 'display: inline-block; border:solid; width:"+width+"px; height: "+width+"px; background-color: red;'</div>");
+  }
 });
 
