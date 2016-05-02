3123f0583963d8dacd2aa72b25b75200200bcde9
Grzegorz <gb87@wp.pl>
2016-05-02 22:19:00 +0200

    Add option to change number of squares and use random colors on hover

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,30 @@
-$(document).ready(function(){
-  width = $('.container').width()/16-6;
+function reset(){ 
+  $('.container > div').css("background-color", "red");
+};
 
-  for (i=1; i <= Math.pow(16,2) ; i++) {
-  $('.container').append("<div style = 'display: inline-block; border:solid; width:"+width+"px; height: "+width+"px; background-color: red;'</div>");
+function addColor() {
+  $(this).css("background-color",get_random_color);
+}
+
+function get_random_color() {
+    var letters = '0123456789ABCDEF'.split('');
+    var color = '#';
+    for (var i = 0; i < 6; i++ ) {
+        color += letters[Math.round(Math.random() * 15)];
+    }
+    return color;
+}
+
+$(document).ready(function(){
+  x = prompt("Give me number of squares in one row, please.");
+  width = $('.container').width()/x;
+
+  for (i=1; i <= Math.pow(x,2) ; i++) {
+  $('.container').append("<div style = 'display: inline-block; width:"+width+"px; height: "+width+"px; background-color: red;'</div>");
   }
   
-  $('.container > div').hover(function() {
-    $(this).css("background-color","black");
-  });
+  $('.container > div').hover(addColor);
 
+  $('button').on('click', reset);
 });
 
