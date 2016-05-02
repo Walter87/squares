0e721744079e6bc2aa883786dda6e223084a6401
Grzegorz <gb87@wp.pl>
2016-05-02 20:37:02 +0200

    Set up hover effect for changing the color of the square

--- scripts/main.js
+++ scripts/main.js
@@ -4,5 +4,10 @@ $(document).ready(function(){
   for (i=1; i <= Math.pow(16,2) ; i++) {
   $('.container').append("<div style = 'display: inline-block; border:solid; width:"+width+"px; height: "+width+"px; background-color: red;'</div>");
   }
+  
+  $('.container > div').hover(function() {
+    $(this).css("background-color","black");
+  });
+
 });
 
